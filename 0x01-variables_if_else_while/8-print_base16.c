ba5c51697743e7bccde761459107840bdfdba14f
abeliumh <ets002012feng@aastu.edu.et>
2022-06-03 22:27:11 +0300

    commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	int i;
-	
+
 	char c;
 	for (i = '0'; i <= '9'; i++)
 		putchar(i);
-	for(c = 'a'; c <= 'f'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 		putchar(c);
 	putchar('\n');
 
