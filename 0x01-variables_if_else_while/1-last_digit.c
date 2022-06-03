20741be54fd2de0eb45175ae7192f4929f0e65af
abeliumh <ets002012feng@aastu.edu.et>
2022-06-03 21:50:39 +0300

    my commit[A

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,13 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if ((n % 10) > 5)
-printf("%s %d %s %d %s\n", "Last digit of", n, "is", n % 10, "and is greater than 5");
+printf("%s %d %s %d %s\n", "Last digit of", n, "is", 
+		n % 10, "and is greater than 5");
 else if ((n % 10) == 0)
-printf("%s %d %s %d %s\n", "Last digit of", n, "is", n % 10, "and is equal to 0");
+printf("%s %d %s %d %s\n", "Last digit of", n, "is",
+		n % 10, "and is equal to 0");
 else if ((n % 10) < 6)
-printf("%s %d %s %d %s\n", "Last digit of", n, "is", n % 10, "and is less than 6 and not 0");
+printf("%s %d %s %d %s\n", "Last digit of", n, "is", n % 10,
+		"and is less than 6 and not 0");
 return (0);
 }
