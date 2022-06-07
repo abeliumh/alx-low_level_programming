725c9ae1f62dcda04a27c2ca3a00965de6b81ef5
abeliumh <ets002012feng@aastu.edu.et>
2022-06-08 02:16:04 +0300

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,30 +6,42 @@
  */
 void print_times_table(int n)
 {
-	if (n >= 0 && n <= 15)
-    	{
-        	int i, j, r;
-        	for (i = 0; i <= n; i++)
-        	{
-            		for (j = 0; j < n; j++)
-            		{
-                		r = (i * j);
-                		_pi * j + '0');
-				_putchar < 10)
+	int i, j, res;
+
+	if (!(n > 15 || n < 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				res = (i * j);
+				if (j != 0)
 				{
-                    			_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
-                		else if (r < 100)
-	_putchar(' ');
+					_putchar(',');
 					_putchar(' ');
 				}
-                		else if (r < 1000)
-                    			_putchar(' ');
-            		}
-            		_putchar(i * n + '0');
-            		_putchar('\n');
-        	}
-    }
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
