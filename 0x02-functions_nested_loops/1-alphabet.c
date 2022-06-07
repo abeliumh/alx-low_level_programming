3245cb4f6637c985fb4b9649c340436155b89bef
abeliumh <ets002012feng@aastu.edu.et>
2022-06-08 01:16:52 +0300

    my commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * print_alphabet - prints lowercase letters.
- * Return: always 0 (success).
- */
-void print_alphabet(void)
-{
-	char i;
 
-	for (i = 'a' ; i <= 'z' ; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
