e2c5b9461743d20d760e2ce53354e6728b6c9212
abeliumh <ets002012feng@aastu.edu.et>
2022-09-06 21:26:54 +0300

    all commit

--- alx-low_level_programming/main.h
+++ alx-low_level_programming/main.h
@@ -1,5 +1,6 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef DYNAMIC_LIB
+#define DYNAMIC_LIB
+#include<stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -15,11 +16,11 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-[Ichar *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* _MAIN_H_ */
+#endif
